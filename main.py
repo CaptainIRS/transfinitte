114a9a1e57b67ad5e51b8a3360c7ea0ac2e92514
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2022-10-15 04:20:02 +0530

    Initial pdf parsing script

---
+++ main.py
@@ -0,0 +1,98 @@
+import cv2
+import numpy as np
+from PIL import Image
+import pytesseract as ts
+from pdf2image import convert_from_path
+import csv
+pages = convert_from_path('/tmp/ac125274.pdf', 500)
+
+texts = []
+
+
+def chop_image(image, page_no):
+    '''
+    Chop image into rows and columns
+    '''
+    global texts
+    width, height = image.size
+    image = np.asarray(image)
+    original = image.copy()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(gray, 230, 255, cv2.THRESH_BINARY_INV)[1]
+    cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    image_number = 0
+    min_area = 10000
+    for c in reversed(cnts):
+        area = cv2.contourArea(c)
+        if area > min_area:
+            x, y, w, h = cv2.boundingRect(c)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (36, 255, 12), 2)
+            new_image = Image.fromarray(original[y:y+h, x:x+w])
+            image_number += 1
+
+            width, height = new_image.size
+            print(new_image.size)
+            if width > 3000:
+                columns = 3
+            elif width > 1500:
+                columns = 2
+            else:
+                columns = 1
+            piece_width = width / columns
+            piece_height = height
+            for column in range(columns):
+                box = (
+                    column * piece_width,
+                    0,
+                    (column + 1) * piece_width,
+                    piece_height
+                )
+                new_image.crop(box).save('cuts/a.png')
+                cut = Image.fromarray(cv2.resize(np.asarray(new_image.crop(box).crop(
+                    (16, 84, 917, 478))), None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA))
+                cut.save('cuts/{}_{}_{}.png'.format(page_no, image_number, column))
+
+                # epic = cv2.resize(np.asarray(new_image.crop(box).crop((900, 10, 1250, 97))), None, fx=2, fy=1, interpolation=cv2.INTER_AREA)
+                # for i in range(len(epic)):
+                #     for j in range(len(epic[i])):
+                #         if sum(epic[i][j]) > 300:
+                #             epic[i][j] = [255, 255, 255]
+                # epic = Image.fromarray(np.asarray(epic))
+                # epic.save('epics/{}_{}_{}.png'.format(page_no, image_number, column))
+
+                deleted = cut.copy()
+                for i in range(len(epic)):
+                    for j in range(len(epic[i])):
+                        if sum(epic[i][j]) > 300:
+                            epic[i][j] = [255, 255, 255]
+                epic = Image.fromarray(np.asarray(epic))
+                epic.save(
+                    'epics/{}_{}_{}.png'.format(page_no, image_number, column))
+
+                text = ts.image_to_string(cut, lang='tam+eng')
+                # epic_text = ts.image_to_string(epic, lang='eng', config="-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ -c tosp_min_sane_kn_sp=0 --psm 8")
+                print('Done', (image_number, column))
+                texts.append((page_no, image_number, column, text))
+
+
+def main():
+    '''
+    Main function
+    '''
+    global texts
+    i = 1
+    for page in pages:
+        if i <= 2:
+            i += 1
+            continue
+        print("Processing page {}".format(i))
+        chop_image(page, i)
+        i += 1
+    with open('output.csv', 'w') as f:
+        csvwriter = csv.writer(f)
+        for text in texts:
+            csvwriter.writerow(text)
+
+
+main()
