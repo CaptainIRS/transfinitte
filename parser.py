217599d04213c2204ce4972c93c8963b784b9e05
anirudhvs <54744932+anirudhvs@users.noreply.github.com>
2022-10-15 10:35:07 +0530

    added parser for eng and tam

---
+++ parser.py
@@ -0,0 +1,158 @@
+from typing import List, Dict
+import csv
+import logging
+from indictrans import Transliterator
+import re
+csv_file_path = 'output.csv'
+
+
+def translate_to_eng(string: str, language: str):
+    if language == 'english':
+        return string
+    if language == 'tamil':
+        return Transliterator(source='tam', target='eng', build_lookup=True).transform(string)
+
+
+def get_relationship_type(relation: str, language: str = 'english') -> str:
+    if language == 'english':
+        return relation.lower()
+    elif language == 'tamil':
+        if 'தந்தை' in relation:
+            return 'father'
+        elif 'தாய்' in relation:
+            return 'mother'
+        elif 'கணவர்' in relation:
+            return 'husband'
+        elif 'மனைவி' in relation:
+            return 'wife'
+        else:
+            return 'other'
+    return 'other'
+
+
+def get_gender(gender: str, language: str = 'english') -> str:
+    if language == 'english':
+        return gender.lower()
+    elif language == 'tamil':
+        if 'ஆண்' in gender:
+            return 'male'
+        elif 'பெண்' in gender:
+            return 'female'
+        else:
+            return 'other'
+    return 'other'
+
+
+def extract_name(lines: List[str], language='english'):
+    if not lines:
+        return '---', []
+    name = lines[0].split(':')
+    slice_index = 1
+    if len(name) != 2:
+        name = '---'
+    else:
+        name = name[1].strip()
+
+    # check if name continues to next line
+    if len(lines) > 1 and ':' not in lines[1]:
+        name = name + ' ' + lines[1].strip()
+        slice_index = 2
+
+    name = translate_to_eng(name, language)
+    name = re.sub('[-]+', '', name).strip()
+    return name, lines[slice_index:]
+
+
+def extract_relationship(lines: List[str], language='english'):
+    if not lines:
+        return '---', '---', []
+    relation_name = lines[0].split(':')
+    slice_index = 1
+    if len(relation_name) != 2:
+        relation_name = '---'
+        relation_type = get_relationship_type(relation_name[0], language)
+    else:
+        relation_type = get_relationship_type(relation_name[0], language)
+        relation_name = relation_name[1].strip()
+
+    # check if name continues to next line
+    if len(lines) > 1 and ':' not in lines[1]:
+        relation_name = relation_name + ' ' + lines[1].strip()
+        slice_index = 2
+    relation_name = translate_to_eng(relation_name, language)
+    relation_name = re.sub('[-]+', '', relation_name).strip()
+    return relation_name, relation_type, lines[slice_index:]
+
+
+def extract_house_number(lines: List[str], language='english'):
+    if not lines:
+        return '---', []
+    house_number = lines[0].split(':')
+    slice_index = 1
+    if len(house_number) != 2:
+        house_number = '---'
+    else:
+        house_number = house_number[1].strip()
+    house_number = translate_to_eng(house_number, language)
+    return house_number, lines[slice_index:]
+
+
+def extract_age_and_gender(lines: List[str], language='english'):
+    if not lines:
+        return '---', '---'
+    age = lines[0].split(':')
+    if len(age) != 3:
+        age = '---'
+    else:
+        age = age[1].strip().split(' ')[0]
+
+    gender = lines[0].split(':')
+    if len(gender) != 3:
+        gender = '---'
+    else:
+        gender = get_gender(gender[2].strip(), language)
+    return age, gender
+
+
+def parse_text(text, language='english') -> dict:
+    # splitlines
+    lines = text.splitlines()
+    # remove empty lines
+    lines = [line for line in lines if line]
+    # remove lines with only whitespace
+    lines = [line.strip() for line in lines if line.strip()]
+    voter_details = dict()
+    try:
+        name_info = extract_name(lines, language)
+        voter_details['name'] = name_info[0]
+        remaining_data = name_info[1]
+        relation_name, relationship_type, remaining_data = extract_relationship(
+            remaining_data, language)
+        voter_details['relation_name'] = relation_name
+        voter_details['relationship_type'] = relationship_type
+
+        voter_details['house_number'], remaining_data = extract_house_number(
+            remaining_data, language)
+
+        age, gender = extract_age_and_gender(remaining_data, language)
+        voter_details['age'] = age
+        voter_details['gender'] = gender
+        return voter_details
+    except Exception as e:
+        # print traceback
+        logging.exception(e)
+        # return {}
+        raise e
+
+
+if __name__ == '__main__':
+    with open('output.csv') as f:
+        # read csv
+        reader = csv.reader(f, delimiter=',')
+        i = 0
+        for row in reader:
+            # if i == 1:
+            #     break
+            i += 1
+            voter_details = parse_text(row[3], 'tamil')
+            print(voter_details)
