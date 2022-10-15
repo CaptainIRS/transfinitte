fc77ad91748dc830c0abbdf0c8986c92c59e5742
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2022-10-15 17:41:29 +0530

    Batch processing

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from PIL import Image
 import pytesseract as ts
 from pdf2image import convert_from_path
 import csv
-from multiprocessing import Pool
-
-pages = convert_from_path('/tmp/ac125274.pdf', 500)
+from multiprocessing import Pool, cpu_count
+from concurrent.futures import ThreadPoolExecutor
+import os
+import urllib.request
+import json
+import logging
+logging.basicConfig(filename='main.log', format='%(asctime)s %(levelname)-8s %(message)s', level=logging.DEBUG)
+logging.getLogger().addHandler(logging.StreamHandler())
 
 def chop_image(image, page_no):
     '''
@@ -48,7 +53,7 @@ def chop_image(image, page_no):
                 )
                 cut = Image.fromarray(cv2.resize(np.asarray(new_image.crop(box).crop(
                     (16, 84, 917, 478))), None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA))
-                cut.save('cuts/{}_{}_{}.png'.format(page_no, image_number, column))
+                # cut.save('cuts/{}_{}_{}.png'.format(page_no, image_number, column))
 
                 # epic = cv2.resize(np.asarray(new_image.crop(box).crop((900, 10, 1250, 97))), None, fx=2, fy=1, interpolation=cv2.INTER_AREA)
                 # for i in range(len(epic)):
@@ -64,26 +69,42 @@ def chop_image(image, page_no):
                 rotationMatrix = cv2.getRotationMatrix2D((dw / 2, dh / 2), -25, 1)
                 deleted = cv2.warpAffine(deleted, rotationMatrix, (dw, dh))
                 deleted = Image.fromarray(deleted)
-                deleted.save(
-                    'deleted/{}_{}_{}.png'.format(page_no, image_number, column))
+                # deleted.save(
+                #     'deleted/{}_{}_{}.png'.format(page_no, image_number, column))
                 deleted_text = ts.image_to_string(deleted, config="-c tessedit_char_whitelist=delt --psm 1")
                 if 'deleted' in deleted_text.lower():
-                    print('Deleted', (image_number, column))
+                    logging.info(f'Deleted {image_number} {column}')
                     continue
 
                 text = ts.image_to_string(cut, lang='tam+eng')
                 # epic_text = ts.image_to_string(epic, lang='eng', config="-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ -c tosp_min_sane_kn_sp=0 --psm 8")
-                print('Done', (page_no, image_number, column))
                 tuples.append((page_no, image_number, column, text))
+    logging.info(f'Done page {page_no}')
     return tuples
 
 
 def main():
-    with Pool(4) as p:
-        texts = p.starmap(chop_image, [(pages[i], i + 1) for i in range(2, len(pages) - 1)])
-        with open('output.csv', 'w') as f:
-            csvwriter = csv.writer(f)
-            for text in texts:
-                csvwriter.writerows(text)
+    parts = json.load(open('parts.json'))
+            
+    start_batch = 0
+    def retrieve(dt, ac, pt):
+        urllib.request.urlretrieve(f'https://www.elections.tn.gov.in/SSR2022_MR_05012022/dt{dt}/ac{ac:03d}/ac{ac:03d}{pt:03d}.pdf', f'rolls/{dt}_{ac}_{pt}.pdf')
+        logging.info(f'Downloaded {dt}_{ac}_{pt}')
+    for batch in range(start_batch, len(parts), 10):
+        logging.info(f'Batch {batch}')
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            for _ in executor.map(lambda x: retrieve(*x), parts[batch:batch+10]):
+                pass
+        for dt, ac, pt in parts[batch:batch+10]:
+            logging.info(f'Parsing {dt}_{ac}_{pt}')
+            pages = convert_from_path(f'rolls/{dt}_{ac}_{pt}.pdf', 500)
+            with Pool(cpu_count() // 2) as p:
+                texts = p.starmap(chop_image, [(pages[i], i + 1) for i in range(2, len(pages) - 1)])
+                with open('output.csv', 'a') as f:
+                    csvwriter = csv.writer(f)
+                    for text in texts:
+                        csvwriter.writerows(text)
+        for dt, ac, pt in parts[batch:batch+10]:
+            os.remove(f'rolls/{dt}_{ac}_{pt}.pdf')
 
 main()
