f15fed7b71c63912c282f4eadfe29da2e8ee98d2
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2022-10-15 15:12:20 +0530

    Add processpool

--- main.py
+++ main.py
@@ -4,16 +4,14 @@ from PIL import Image
 import pytesseract as ts
 from pdf2image import convert_from_path
 import csv
+from multiprocessing import Pool
+
 pages = convert_from_path('/tmp/ac125274.pdf', 500)
-
-texts = []
-
 
 def chop_image(image, page_no):
     '''
     Chop image into rows and columns
     '''
-    global texts
     width, height = image.size
     image = np.asarray(image)
     original = image.copy()
@@ -23,6 +21,7 @@ def chop_image(image, page_no):
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
     image_number = 0
     min_area = 10000
+    tuples = []
     for c in reversed(cnts):
         area = cv2.contourArea(c)
         if area > min_area:
@@ -32,7 +31,6 @@ def chop_image(image, page_no):
             image_number += 1
 
             width, height = new_image.size
-            print(new_image.size)
             if width > 3000:
                 columns = 3
             elif width > 1500:
@@ -48,7 +46,6 @@ def chop_image(image, page_no):
                     (column + 1) * piece_width,
                     piece_height
                 )
-                new_image.crop(box).save('cuts/a.png')
                 cut = Image.fromarray(cv2.resize(np.asarray(new_image.crop(box).crop(
                     (16, 84, 917, 478))), None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA))
                 cut.save('cuts/{}_{}_{}.png'.format(page_no, image_number, column))
@@ -61,38 +58,32 @@ def chop_image(image, page_no):
                 # epic = Image.fromarray(np.asarray(epic))
                 # epic.save('epics/{}_{}_{}.png'.format(page_no, image_number, column))
 
-                deleted = cut.copy()
-                for i in range(len(epic)):
-                    for j in range(len(epic[i])):
-                        if sum(epic[i][j]) > 300:
-                            epic[i][j] = [255, 255, 255]
-                epic = Image.fromarray(np.asarray(epic))
-                epic.save(
-                    'epics/{}_{}_{}.png'.format(page_no, image_number, column))
+                # deleted = np.asarray(cut).copy()
+                dw, dh = cut.size
+                deleted = cv2.threshold(np.asarray(cut), 20, 255, cv2.THRESH_BINARY_INV)[1]
+                rotationMatrix = cv2.getRotationMatrix2D((dw / 2, dh / 2), -25, 1)
+                deleted = cv2.warpAffine(deleted, rotationMatrix, (dw, dh))
+                deleted = Image.fromarray(deleted)
+                deleted.save(
+                    'deleted/{}_{}_{}.png'.format(page_no, image_number, column))
+                deleted_text = ts.image_to_string(deleted, config="-c tessedit_char_whitelist=delt --psm 1")
+                if 'deleted' in deleted_text.lower():
+                    print('Deleted', (image_number, column))
+                    continue
 
                 text = ts.image_to_string(cut, lang='tam+eng')
                 # epic_text = ts.image_to_string(epic, lang='eng', config="-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ -c tosp_min_sane_kn_sp=0 --psm 8")
-                print('Done', (image_number, column))
-                texts.append((page_no, image_number, column, text))
+                print('Done', (page_no, image_number, column))
+                tuples.append((page_no, image_number, column, text))
+    return tuples
 
 
 def main():
-    '''
-    Main function
-    '''
-    global texts
-    i = 1
-    for page in pages:
-        if i <= 2:
-            i += 1
-            continue
-        print("Processing page {}".format(i))
-        chop_image(page, i)
-        i += 1
-    with open('output.csv', 'w') as f:
-        csvwriter = csv.writer(f)
-        for text in texts:
-            csvwriter.writerow(text)
-
+    with Pool(4) as p:
+        texts = p.starmap(chop_image, [(pages[i], i + 1) for i in range(2, len(pages) - 1)])
+        with open('output.csv', 'w') as f:
+            csvwriter = csv.writer(f)
+            for text in texts:
+                csvwriter.writerows(text)
 
 main()
